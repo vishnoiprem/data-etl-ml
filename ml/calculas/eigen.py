a0ed48b206a6ffca8a8d888a8381e52a587dce9f
vishnoiprem <5586954+vishnoiprem@users.noreply.github.com>
2024-08-22 23:56:40 +0700

    add

---
+++ ml/calculas/eigen.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+# Define a 2x2 matrix
+A = np.array([[4, 2],
+              [1, 3]])
+
+# Compute the eigenvalues and eigenvectors
+eigenvalues, eigenvectors = np.linalg.eig(A)
+
+print("Matrix A:")
+print(A)
+
+print("\nEigenvalues:")
+print(eigenvalues)
+
+print("\nEigenvectors:")
+print(eigenvectors)
+
+import numpy as np
+
+# Define a 2x2 matrix
+A = np.array([[4, 2],
+              [1, 3]])
+
+# Compute the eigenvalues and eigenvectors
+eigenvalues, eigenvectors = np.linalg.eig(A)
+
+print("Matrix A:")
+print(A)
+
+print("\nEigenvalues:")
+print(eigenvalues)
+
+print("\nEigenvectors:")
+print(eigenvectors)
