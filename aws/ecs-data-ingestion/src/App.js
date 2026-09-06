3d827c670caef4704d2a8d66d8fcfaf4daa3e17e
premvishnoi <pvishnoi@cpaxtra.co.th>
2026-09-06 23:54:51 +0700

    code

---
+++ aws/ecs-data-ingestion/src/App.js
@@ -0,0 +1,22 @@
+import logo from './ecs.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <b>
+            Congratulations 🎉
+          </b>
+        </p>
+        <p>
+        Your React application is successfully running on <b>Amazon ECS</b> 🚀🔥
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
